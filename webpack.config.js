609ac8a0a1d00171e1b6977162217adfcb5bf97a
andrey <talash10@mail.ru>
2023-07-07 00:46:48 +0300

    add docs dir

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: 'index.js'
   },
   module: {
